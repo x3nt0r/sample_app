0054d8f8996c3d2e90c6af2f772ed3d142e2b58a
Xentor <alban.degiuli@gmail.com>
2014-08-11 18:18:32 +0200

    page statique step 1

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       expect(response).to be_success
+    end
+
+
+    it "devrait avoir le bon titre" do
+      get 'home'
+      response.should have_selector("title",
+                        :content => "Simple App du Tutoriel Ruby on Rails | Accueil")
     end
   end
 
@@ -14,6 +22,28 @@ RSpec.describe PagesController, :type => :controller do
       get 'contact'
       expect(response).to be_success
     end
+
+
+    it "devrait avoir le bon titre" do
+      get 'contact'
+      response.should have_selector("title",
+                        :content => "Simple App du Tutoriel Ruby on Rails | Contact")
+    end
   end
+
+  describe "GET 'about'" do
+    it "returns http success" do
+      get 'about'
+      expect(response).to be_success
+    end
+
+
+    it "devrait avoir le bon titre" do
+      get 'about'
+      response.should have_selector("title",
+                        :content => "Simple App du Tutoriel Ruby on Rails | A propos")
+    end
+  end
+
 
 end
